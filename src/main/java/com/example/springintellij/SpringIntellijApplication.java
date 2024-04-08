bdbaddc4e5cf90d3cbabdd8139983af95d493a06
Erik sales <58636303+ErickSousa312@users.noreply.github.com>
2024-04-08 16:49:18 -0300

    update aplication from .properties to .yml

--- src/main/java/com/example/springintellij/SpringIntellijApplication.java
+++ src/main/java/com/example/springintellij/SpringIntellijApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringIntellijApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringIntellijApplication.class, args);
+    public static void main(String[] args) {  SpringApplication.run(SpringIntellijApplication.class, args);
     }
 
 }
